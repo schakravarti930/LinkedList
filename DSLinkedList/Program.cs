da0fd7faffb1be134677184d30facd7a42b88f7b
Swapnil <schakravarti930@gmail.com>
2020-10-21 13:07:42 +0530

    [Swapnil] Add : Add new node at beginning of list

--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -7,9 +7,11 @@ namespace DSLinkedList
         static void Main(string[] args)
         {
            //Create a simple Linked List
-           Node head = new Node(56);
-           head.next = new Node(30);
-           head.next.next = new Node(70);
+           LinkedList list = new LinkedList();
+           list.Add(70);
+           list.Add(30);
+           list.Add(56);
+           list.Display();
         }
     }
 }
