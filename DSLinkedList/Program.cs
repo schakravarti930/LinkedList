b9d95be930c01afc675509fbd4e4d1f7fd42fff6
Swapnil <schakravarti930@gmail.com>
2020-10-21 14:05:28 +0530

    [Swapnil] Add : Insert node at any given position

--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -11,6 +11,8 @@ namespace DSLinkedList
            list.Append(56);
            list.Append(30);
            list.Append(70);
+           list.Append(100);
+           list.Insert(4,12);
            list.Display();
         }
     }
