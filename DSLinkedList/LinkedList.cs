0dd0e4ee059cac74e84f06002d5a70c892725409
Swapnil <schakravarti930@gmail.com>
2020-10-21 14:29:52 +0530

    [Swapnil] Add : Remove Last Node in List

--- DSLinkedList/LinkedList.cs
+++ DSLinkedList/LinkedList.cs
@@ -79,6 +79,25 @@ namespace DSLinkedList
                 return popped;
             }
         }
+        internal Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return null;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next.next != null)
+                {
+                    temp = temp.next;
+                }
+                Node removedNode = temp.next;
+                temp.next = null;
+                return removedNode;
+            }
+        }
         internal void Display()
         {
             if(head == null)
